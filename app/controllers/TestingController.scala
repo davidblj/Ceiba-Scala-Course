d3b0e8e01c149a8dae07821400f9c55c1fc87846
CEIBA\david.jaramillo <david.blj95@gmail.com>
2018-08-22 09:54:01 -0500

    agregar solucion
    Se soluciona el literal 3 de la seccion "Actions Controllers y Resutls"

--- app/controllers/TestingController.scala
+++ app/controllers/TestingController.scala
@@ -11,4 +11,8 @@ class TestingController @Inject() (cc: ControllerComponents) (implicit exec: Exe
   def found = Action { request => Ok("look at this nice and minimalistic controller definition") }
   def badRequest = Action { request => BadRequest("Oops, page not found")}
   def internalServerError = Action { request => InternalServerError("Omg, you just broke this !")}
+  def learnSomeScala = Action { Ok.sendFile(
+    new java.io.File(
+      "C:\\Users\\david.jaramillo\\Desktop\\repos\\Idea workspace\\Starter\\public\\pdf\\essential-scala.pdf"),
+      false)}
 }
