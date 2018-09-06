c18b44058aa06c90cdb58c7a9e286598524f4d7a
davidblj <david.blj95@gmail.com>
2018-09-06 18:00:43 -0500

    agregar solucion
    Se solucionan los literales de la seccion "Filtros"

--- app/filters/ExampleFilter.scala
+++ app/filters/ExampleFilter.scala
@@ -16,7 +16,8 @@ import scala.concurrent.ExecutionContext
 class ExampleFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
   override def apply(next: EssentialAction) = EssentialAction { request =>
     next(request).map { result =>
-      result.withHeaders("X-ExampleFilter" -> "foo")
+      // do some logging also !
+      result.withHeaders("X-Ceiba-filter" -> "foo")
     }
   }
 }
